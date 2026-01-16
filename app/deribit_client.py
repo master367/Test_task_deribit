3cf03b3933a8e9dc3393bc483a4c3397152e5d69
master367 <adiko7634@gmail.com>
2026-01-17 01:21:52 +0500

    4

--- app/deribit_client.py
+++ app/deribit_client.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 class DeribitClient:
     def __init__(self):
         self.base_url = "https://test.deribit.com/api/v2/public/get_index_price"
-        # Добавляем заголовки, чтобы имитировать браузер
+
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
@@ -14,7 +14,7 @@ class DeribitClient:
     async def get_index_price(self, ticker: str):
         params = {"index_name": ticker}
         try:
-            # Устанавливаем таймаут, чтобы запрос не висел вечно
+
             timeout = aiohttp.ClientTimeout(total=15)
             async with aiohttp.ClientSession(headers=self.headers, timeout=timeout) as session:
                 async with session.get(self.base_url, params=params) as response:
